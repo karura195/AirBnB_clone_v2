042ce84150dc59a6e0b6dd41031889717606990b
karura195 <karla.diaz@pucp.pe>
2021-12-21 16:29:13 -0800

    Delete updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,15 +42,16 @@ class FileStorage:
 			temp = {}
 			temp.update(FileStorage.__objects)
 			for key, val in temp.items():
-				temp[key] = val.to_dict()
-				json.dump(temp, f)
+                            temp[key] = val.to_dict()
+			json.dump(temp, f)
 
 
 	def delete(self, obj=None):
 		"""Para eliminar obj de __objects si está dentro"""
-		if obj:
-			key = "{}.{}".format(type(obj).__name__, obj.id)
-			del self.__objects[key]
+		if not obj:
+                    return
+		key = "{}.{}".format(type(obj).__name__, obj.id)
+		del FileStorage.__objects[key]
 
 	def reload(self):
 		"""Loads storage dictionary from file"""

66e2163c9ef4b72034470bf22ca987302cb25196
karura195 <karla.diaz@pucp.pe>
2021-12-22 10:38:20 -0800

    Upload classes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
+from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import String
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    if models.is_db == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City')
+    else:
+        name = ""
+        cities = models.storage.all(City)

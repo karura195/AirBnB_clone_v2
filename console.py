6be576b7267f5e76ab79b8c0d0b605a380813f77
karura195 <karla.diaz@pucp.pe>
2021-12-25 21:01:30 -0800

    Console fixed

--- console.py
+++ console.py
@@ -222,7 +222,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
-       print_list = []
+        print_list = []
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
             if args not in HBNBCommand.classes:
